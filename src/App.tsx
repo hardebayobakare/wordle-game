176276df11398e7871b7cb017c20b4799bead086
Adebayo <hardebayobakare@gmail.com>
2025-01-07 16:09:52 -0500

    update ThemeSwitch component

--- src/App.tsx
+++ src/App.tsx
@@ -216,7 +216,7 @@ function App() {
               onClick={() => setShowInstructions(true)}
               aria-label="Help"
             >
-              {/* <svg
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="24"
                 viewBox="0 0 24 24"
@@ -226,27 +226,7 @@ function App() {
                   fill="currentColor"
                   d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
                 ></path>
-              </svg> */}
-              {darkMode ? (
-                <svg width="36" height="32" viewBox="0 0 36 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M3.87868 4.87868C3 5.75736 3 7.17157 3 10V22C3 24.8284 3 26.2426 3.87868 27.1213C4.75736 28 6.17157 28 9 28H27C29.8284 28 31.2426 28 32.1213 27.1213C33 26.2426 33 24.8284 33 22V10C33 7.17157 33 5.75736 32.1213 4.87868C31.2426 4 29.8284 4 27 4H9C6.17157 4 4.75736 4 3.87868 4.87868Z"
-                    fill="white"
-                  />
-                  <path d="M24 11C24.5523 11 25 11.4477 25 12V22.6667C25 23.219 24.5523 23.6667 24 23.6667C23.4477 23.6667 23 23.219 23 22.6667V12C23 11.4477 23.4477 11 24 11Z" fill="white" />
-                  <path d="M13 14.6667C13 14.1144 12.5523 13.6667 12 13.6667C11.4477 13.6667 11 14.1144 11 14.6667V22.6667C11 23.219 11.4477 23.6667 12 23.6667C12.5523 23.6667 13 23.219 13 22.6667V14.6667Z" fill="white" />
-                  <path d="M19 17.3333C19 16.781 18.5523 16.3333 18 16.3333C17.4477 16.3333 17 16.781 17 17.3333V22.6667C17 23.219 17.4477 23.6667 18 23.6667C18.5523 23.6667 19 23.219 19 22.6667V17.3333Z" fill="white" />
-                </svg>
-              ) : (
-                <svg width="40" height="36" viewBox="0 0 40 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="4.93549" y="6" width="29.6129" height="24" rx="2" fill="black" fillOpacity="0.49" />
-                  <path d="M13.1613 15L13.1613 24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                  <path d="M19.7419 18V24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                  <path d="M26.3226 12V24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-              )}
+              </svg>
             </IconButton>
           </HeaderSection>
           <HeaderSection>
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -52,9 +52,26 @@ const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ checked, onChange }) => {
   return (
     <Container>
       <StatsIcon>
-        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20">
-          <path fill="currentColor" d="M16,11V3H8v6H2v12h20V11H16z M10,5h4v14h-4V5z M4,11h4v8H4V11z M20,19h-4v-6h4V19z"></path>
-        </svg>
+        {checked ? (
+          <svg width="36" height="32" viewBox="0 0 36 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M3.87868 4.87868C3 5.75736 3 7.17157 3 10V22C3 24.8284 3 26.2426 3.87868 27.1213C4.75736 28 6.17157 28 9 28H27C29.8284 28 31.2426 28 32.1213 27.1213C33 26.2426 33 24.8284 33 22V10C33 7.17157 33 5.75736 32.1213 4.87868C31.2426 4 29.8284 4 27 4H9C6.17157 4 4.75736 4 3.87868 4.87868Z"
+              fill="white"
+            />
+            <path d="M24 11C24.5523 11 25 11.4477 25 12V22.6667C25 23.219 24.5523 23.6667 24 23.6667C23.4477 23.6667 23 23.219 23 22.6667V12C23 11.4477 23.4477 11 24 11Z" fill="white" />
+            <path d="M13 14.6667C13 14.1144 12.5523 13.6667 12 13.6667C11.4477 13.6667 11 14.1144 11 14.6667V22.6667C11 23.219 11.4477 23.6667 12 23.6667C12.5523 23.6667 13 23.219 13 22.6667V14.6667Z" fill="white" />
+            <path d="M19 17.3333C19 16.781 18.5523 16.3333 18 16.3333C17.4477 16.3333 17 16.781 17 17.3333V22.6667C17 23.219 17.4477 23.6667 18 23.6667C18.5523 23.6667 19 23.219 19 22.6667V17.3333Z" fill="white" />
+          </svg>
+        ) : (
+          <svg width="40" height="36" viewBox="0 0 40 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="4.93549" y="6" width="29.6129" height="24" rx="2" fill="black" fillOpacity="0.49" />
+            <path d="M13.1613 15L13.1613 24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M19.7419 18V24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M26.3226 12V24" stroke="white" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        )}
       </StatsIcon>
       <Switch data-checked={checked} onClick={onChange}>
         <Handle checked={checked} />
